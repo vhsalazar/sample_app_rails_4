5595da04d77edd3ef0da222f638ef94cd6c9ee4c
Victor H. Salazar <victor@ninjadevs.com>
2014-08-27 12:46:47 -0400

    changed unicorn|add config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,47 +1,66 @@
-# paths
-app_path = "/var/www"
-working_directory "#{app_path}/my_app"
-pid               "#{app_path}/my_app/tmp/pids/unicorn.pid"
+# # paths
+# app_path = "/var/www"
+# working_directory "#{app_path}/my_app"
+# pid               "#{app_path}/my_app/shared/tmp/pids/unicorn.pid"
 
-# listen
-listen "/tmp/my_app.socket", :backlog => 64
+# # listen
+# listen "/var/www/my_app/shared/my_app.socket", :backlog => 64
 
-# logging
-stderr_path "log/unicorn.stderr.log"
-stdout_path "log/unicorn.stdout.log"
+# # logging
+# stderr_path "log/unicorn.stderr.log"
+# stdout_path "log/unicorn.stdout.log"
 
-# workers
-worker_processes 3
+# # workers
+# worker_processes 3
 
-# use correct Gemfile on restarts
-before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = "#{app_path}/my_app/Gemfile"
-end
+# # use correct Gemfile on restarts
+# before_exec do |server|
+#   ENV['BUNDLE_GEMFILE'] = "#{app_path}/my_app/Gemfile"
+# end
 
-# preload
-preload_app true
+# # preload
+# preload_app true
 
-before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
+# before_fork do |server, worker|
+#   # the following is highly recomended for Rails + "preload_app true"
+#   # as there's no need for the master process to hold a connection
+#   if defined?(ActiveRecord::Base)
+#     ActiveRecord::Base.connection.disconnect!
+#   end
 
-  # Before forking, kill the master process that belongs to the .oldbin PID.
-  # This enables 0 downtime deploys.
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
+#   # Before forking, kill the master process that belongs to the .oldbin PID.
+#   # This enables 0 downtime deploys.
+#   old_pid = "#{server.config[:pid]}.oldbin"
+#   if File.exists?(old_pid) && server.pid != old_pid
+#     begin
+#       Process.kill("QUIT", File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH
+#       # someone else did our job for us
+#     end
+#   end
+# end
 
-after_fork do |server, worker|
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
+# after_fork do |server, worker|
+#   if defined?(ActiveRecord::Base)
+#     ActiveRecord::Base.establish_connection
+#   end
+# end
+
+
+
+root = "/Users/vh5/Documents/Vagrant/capistrano/sample_app_rails_4"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+ 
+# Port configuration
+listen 3000
+worker_processes 2
+timeout 30
+ 
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
 end

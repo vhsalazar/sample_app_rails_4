2e0ea331a926d0b95de0769f7339aa90deffc27e
Victor H. Salazar <victor@ninjadevs.com>
2014-08-27 14:23:14 -0400

    update unicorn config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -19,32 +19,7 @@
 # end
 
 # # preload
-# preload_app true
 
-# before_fork do |server, worker|
-#   # the following is highly recomended for Rails + "preload_app true"
-#   # as there's no need for the master process to hold a connection
-#   if defined?(ActiveRecord::Base)
-#     ActiveRecord::Base.connection.disconnect!
-#   end
-
-#   # Before forking, kill the master process that belongs to the .oldbin PID.
-#   # This enables 0 downtime deploys.
-#   old_pid = "#{server.config[:pid]}.oldbin"
-#   if File.exists?(old_pid) && server.pid != old_pid
-#     begin
-#       Process.kill("QUIT", File.read(old_pid).to_i)
-#     rescue Errno::ENOENT, Errno::ESRCH
-#       # someone else did our job for us
-#     end
-#   end
-# end
-
-# after_fork do |server, worker|
-#   if defined?(ActiveRecord::Base)
-#     ActiveRecord::Base.establish_connection
-#   end
-# end
 
 
 
@@ -66,4 +41,31 @@ timeout 30
 # reference the capistrano "current" symlink
 before_exec do |_|
   ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
+
+preload_app true
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
 end
